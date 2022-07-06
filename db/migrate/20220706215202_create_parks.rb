9afba702f22a9f9ee6f59102b229fd653be998b4
Eli Sachs <easachs13@gmail.com>
2022-07-06 17:06:38 -0600

    Add parks index

---
+++ db/migrate/20220706215202_create_parks.rb
@@ -0,0 +1,11 @@
+class CreateParks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parks do |t|
+      t.string :name
+      t.boolean :affluent
+      t.integer :year
+      
+      t.timestamps
+    end
+  end
+end
